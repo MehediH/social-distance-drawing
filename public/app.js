7c17fabcae5b7a152231b6010745a0a628509d29
MehediH <mehedi.dip@outlook.com>
2020-04-23 21:56:59 +0100

    Better anon names

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 
 
-const { room, name} = Qs.parse(location.search, {
+let { room, name} = Qs.parse(location.search, {
   ignoreQueryPrefix: true
 });
 
@@ -31,9 +31,14 @@ feather.replace() // load icons
 let id = "2412"
 
 socket.on("connect", () => {
-  id = socket.id.substr(0, 5).replace(/[^a-z0-9]/gi,'').toLowerCase()
+  if(!name){ // if user doesn't have a name, we give them a random one
+    let getIndex = (l) =>  Math.floor(Math.random() * (l - 0)) + 0;
+    let adjs = ["alizarin","amaranth","amber","amethyst","apricot","aqua","aquamarine","asparagus","auburn","azure","beige","bistre","black","blue","blue-green","blue-violet","bondi-blue","brass","bronze","brown","buff","burgundy","camouflage-green","cardinal","carmine","carrot-orange","cerise","cerulean","champagne","charcoal","chartreuse","cherry-blossom-pink","chestnut","chocolate","cinnabar","cinnamon","cobalt","copper","coral","corn","cornflower","cream","crimson","cyan","dandelion","denim","ecru","emerald","eggplant","fern-green","firebrick","flax","forest-green","french-rose","fuchsia","gamboge","gold","goldenrod","green","grey","han-purple","harlequin","heliotrope","hollywood-cerise","indigo","ivory","jade","kelly-green","khaki","lavender","lawn-green","lemon","lemon-chiffon","lilac","lime","lime-green","linen","magenta","magnolia","malachite","maroon","mauve","midnight-blue","mint-green","misty-rose","moss-green","mustard","myrtle","navajo-white","navy-blue","ochre","office-green","olive","olivine","orange","orchid","papaya-whip","peach","pear","periwinkle","persimmon","pine-green","pink","platinum","plum","powder-blue","puce","prussian-blue","psychedelic-purple","pumpkin","purple","quartz-grey","raw-umber","razzmatazz","red","robin-egg-blue","rose","royal-blue","royal-purple","ruby","russet","rust","safety-orange","saffron","salmon","sandy-brown","sangria","sapphire","scarlet","school-bus-yellow","sea-green","seashell","sepia","shamrock-green","shocking-pink","silver","sky-blue","slate-grey","smalt","spring-bud","spring-green","steel-blue","tan","tangerine","taupe","teal","terra-cotta","thistle","titanium-white","tomato","turquoise","tyrian-purple","ultramarine","van-dyke-brown","vermilion","violet","viridian","wheat","white","wisteria","yellow","zucchini"];
+
+    name = "wild-" + adjs[getIndex(adjs.length)]
+  }
   
-  joinRoom(name ? name : "anon-" + id)
+  joinRoom(name)
 })
 
 
